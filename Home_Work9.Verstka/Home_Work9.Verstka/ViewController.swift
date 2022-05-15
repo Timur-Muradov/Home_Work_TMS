94dbc35dc0c430dece608d115945aefb079b7e4f
Timur-Muradov <102965817+Timur-Muradov@users.noreply.github.com>
2022-05-15 22:15:05 +0300

    Add files via upload

---
+++ Home_Work9.Verstka/Home_Work9.Verstka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home_Work9.Verstka
+//
+//  Created by Тимур Мурадов on 11.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
